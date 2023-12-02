f38795758ae930bc82801222f552371d814d0137
Zain Zulfiqar <zzainzulfiqar@gmail.com>
2023-12-01 20:37:25 -0500

    Created main menu

---
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.project5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
