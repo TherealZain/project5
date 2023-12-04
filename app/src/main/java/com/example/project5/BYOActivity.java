a0447f2d816998ec462679ba3723f1a47e57d7b4
Zain Zulfiqar <zzainzulfiqar@gmail.com>
2023-12-03 22:47:11 -0500

    Designed Build Your Own activity

--- app/src/main/java/com/example/project5/BYOActivity.java
+++ app/src/main/java/com/example/project5/BYOActivity.java
@@ -1,8 +1,14 @@
 package com.example.project5;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BYOActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class BYOActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_byo);
+        setUpSpinner();
+    }
+
+    private void setUpSpinner(){
+        ListView listView = findViewById(R.id.addToppings);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
     }
 }
